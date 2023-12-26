e1577ca121e2c9b888d2f615cc1ccab8b1fb5891
mavlonbekswd <oqiwkere@gmail.com>
2023-12-26 22:21:15 +0500

    f

---
+++ main.js
@@ -0,0 +1,354 @@
+// 1-masala
+
+// var A=90;
+
+// if (A>0) {
+//     console.log ("musbat")
+// } else {if (A<0){
+//     console.log( 'manfiy')
+// }
+// }
+
+
+// 2-3-masala
+
+// var A = 2;
+
+// if (A % 2 ==1) {
+//     console.log("A toq son");
+// } else {
+//     console.log("A juft son ");
+// }
+
+//4 masala
+
+// var a=3
+// var b=-3
+// if (a > 0) {
+//     console.log(true)
+    
+//     if (b>0) {
+//         console.log (true)
+//     }
+// }else if (a<0) {
+//     console.log(false)
+// }
+// if (b>0) {
+//     console.log (true)
+// }else if (b<0)
+// {
+//     console.log(false)
+// }
+
+// 5-masala
+
+// var a=6
+// var b=4
+// var c=3
+
+// if (a>b) {
+//     console.log (true)
+// } else if (a<b) {
+//     console.log (false)
+// }
+// if (b>c) {
+//     console.log (true)
+// } else if (b<c) {
+//     console.log (false)
+// } 
+
+// 6-masala
+
+// var a=3
+// var b=4
+// var c=5
+
+// if (a>b) {
+//     console.log (true)
+// }
+//  else if (a<b) {
+//     console.log (false)
+// } 
+// if (c<b) {
+//     console.log (true)
+// }
+//  else if (c>b) {
+//     console.log (false)
+// } 
+// if (a>c) {
+//     console.log (true)
+// }
+//  else if (a<c) {
+//     console.log (false)
+// } 
+
+// 7-masala
+
+// var A = 2;
+// var b=3;
+
+// if (A % 2 ==1) {
+//     console.log("A toq son");
+// } else {
+//     console.log("A juft son ");
+// }
+// if (b % 2 ==1) {
+//     console.log("b toq son");
+// } else {
+//     console.log("b juft son ");
+// }
+
+
+//8-masala
+// var a=3
+// var b=4
+// var c=5
+// var d=5
+
+// if (a>0) {
+//     console.log ("musbat")
+// } else {if (a<0){
+//     console.log( 'manfiy')
+// }
+// }
+
+// if (b>0) {
+//     console.log ("musbat")
+// } else {if (b<0){
+//     console.log( 'manfiy')
+// }
+// }
+// if (c>0) {
+//     console.log ("musbat")
+// } else {if (c<0){
+//     console.log( 'manfiy')
+// }
+// }
+// if (d>0) {
+//     console.log ("musbat")
+// } else {if (d<0){
+//     console.log( 'manfiy')
+// }
+// }
+
+// 9-masala
+// var a = -4;
+// var b = -4;
+// var c = -5;
+
+// if (a > 0 && b > 0) {
+//     console.log("true");
+// } else if (a > 0 && c > 0) {
+//     console.log("true");
+// } else if (b > 0 && c > 0) {
+//     console.log("true");
+// } else {
+//     console.log("Ikki yoki undan ortiq son musbat emas");
+// }
+
+//10-masala
+
+// var haftaKunlari = ["Dushanba", "Seshanba", "Chorshanba", "Payshanba", "Juma", "Shanba", "Yakshanba"];
+// var number = 3;
+
+// if (number >= 1 && number <= 7) {
+//     var haftaKuni = haftaKunlari[number - 1];
+//     console.log( haftaKuni);
+// } else{
+//     console.log("false")
+// }
+
+// 11-masala
+
+// var Companiyalar = ["Uzmobile", "Ucell", "Beline", "Humans", "UMS"];
+// var number = 93;
+
+// if (number >= 90 && number <= 95) {
+//     var Company = Companiyalar[number - 1];
+//     console.log( Company);
+// } else{
+//     console.log("false")
+// }
+
+// 12-masala
+
+// var butunSon= 7
+
+// if (butunSon > 0 + 1) {
+//     console.log(true)
+// }else if (butunSon < 0 - 1) {
+//     console.log(false)
+// }
+
+// 13-masala
+
+// var butunSon=6
+
+// if (butunSon > 0 + 3) {
+//     console.log(true)
+// }else if (butunSon < 0 - 2) {
+//     console.log(false)
+// }
+
+// 14-masala
+
+// var a=4
+// var b=4
+
+// if ( a>= b) {
+//     console.log("a katta yoki teng")
+// } else if ( a<= b) {
+//     console.log("b katta yoki teng")
+// }
+
+// 15-masala
+
+// var a=4
+// var b=2
+// var c=1
+
+// if ( a>= b) {
+//     console.log("a katta yoki teng")
+// } else if ( a<= b) {
+//     console.log("b katta yoki teng")
+// } 
+//  if ( a<= c) {
+//     console.log("c katta yoki teng")
+// } else if ( a >= c) {
+//     console.log("a katta yoki teng")
+// }
+//  if ( b <= c) {
+//     console.log("c katta yoki teng")
+// } else if ( b >= c) {
+//     console.log("b katta yoki teng")
+// }
+
+
+ //16-masala
+
+
+ // var a=4
+// var b=2
+// var c=1
+
+// if ( a<= b) {
+//     console.log("a kichik yoki teng")
+// } else if ( a>= b) {
+//     console.log("b kichik yoki teng")
+// } 
+//  if ( a>= c) {
+//     console.log("c kichik yoki teng")
+// } else if ( a <= c) {
+//     console.log("a kichik yoki teng")
+// }
+//  if ( b <= c) {
+//     console.log("b kichik yoki teng")
+// } else if ( b >= c) {
+//     console.log("c kichik yoki teng")
+// }
+
+
+// 18-masala
+
+// var  a = 4;
+
+// if (a >= 4 && a <= 5) {
+//     console.log(" Stipendiya beriladi")
+// } else if (a <= 3 ) {
+//     console.log(" stipendiya berilmaydi");
+// } 
+
+
+// 19-masala
+
+// var A=4;
+
+// if (A>0) {
+//     console.log ("musbat")
+// } else {if (A<0){
+//     console.log( 'manfiy')
+// }
+// }
+
+// if (A % 2 ==1) {
+//     console.log("A toq son");
+// } else {
+//     console.log("A juft son ");
+// }
+
+
+//21-masala
+
+// var A=3
+// var B=4
+// var C=5
+
+// if (A <= B && B<= C) {
+//     console.log("tt")
+// } else if (A >= B || B <= C) {
+//    console.log("nt")
+// }
+
+// 22-masala
+
+// var A = 2;
+// var b=3;
+
+// if (A % 2 ==1) {
+//     console.log("A toq son");
+// } else  {
+//     console.log("A toq son emas ");
+// }
+// if (b % 2 ==1) {
+//     console.log("b toq son");
+// } else {
+//     console.log("b toq son emas ");
+// }
+
+// 23-masala
+
+// var A=90;
+// var b=3
+// var c =4
+
+// if (A>0 && b>0 && c>0){
+//     console.log ("barchasi musbat")
+// } else {
+//     console.log( 'manfiy')
+
+// }
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
